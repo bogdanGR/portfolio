245cfb353eaeaa5561f091a2e1c88c6818b42dac
bogdanGR <fant1k450@gmail.com>
2025-07-27 18:52:03 +0300

    move the second section from header component to homeView in order to prevent showed it up in every page

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -42,27 +42,4 @@ const typedStrings = [
       </div>
     </div>
   </nav>
-
-  <div class="row pt-5">
-    <div class="home-header col-md-7">
-      <h1 class="heading" style="padding-bottom: 15px;">Hi There! <span class="wave" role="img" aria-labelledby="wave">👋🏻</span></h1>
-      <h1 class="heading-name">I'M<strong class="main-name"> Bogdan Vaskan</strong></h1>
-      <div class="pt-2" style="text-align: left;">
-        <div class="Typewriter d-flex" data-testid="typewriter-wrapper">
-          <VueTyped
-            :strings="typedStrings"
-            :typeSpeed="40"
-            :backSpeed="30"
-            :loop="true"
-            @onComplete="handleComplete"
-          ></VueTyped>
-          <span class="Typewriter__cursor">|</span>
-        </div>
-      </div>
-    </div>
-
-    <div class="col-md-5" style="padding-bottom: 20px;">
-      <img src="/src/assets/images/header_image.svg" alt="home pic" class="img-fluid" style="max-height: 450px;">
-    </div>
-  </div>
 </template>
