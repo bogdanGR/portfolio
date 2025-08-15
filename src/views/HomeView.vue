e2eeb634159035c625a974048d843721ac6fce9c
bogdanGR <fant1k450@gmail.com>
2025-08-15 13:31:51 +0300

    remove particles

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -9,29 +9,29 @@ const typedStrings = [
 
 <template>
   <main>
-      <div class="row mt-5 mb-5">
-        <div class="home-header col-md-7">
-          <h1 class="heading" style="padding-bottom: 15px;">Hi There! <span class="wave" role="img" aria-labelledby="wave">👋🏻</span></h1>
-          <h1 class="heading-name">I'M<strong class="main-name"> Bogdan Vaskan</strong></h1>
-          <div class="pt-2" style="text-align: left;">
-            <div class="Typewriter d-flex" data-testid="typewriter-wrapper">
-              <VueTyped
-                :strings="typedStrings"
-                :typeSpeed="40"
-                :backSpeed="30"
-                :loop="true"
-                @onComplete="handleComplete"
-              ></VueTyped>
-              <span class="Typewriter__cursor">|</span>
-            </div>
+    <div id="home-section" class="row mt-5 mb-5 pt-5 pb-5 home-header">
+      <div class=" col-md-7">
+        <h1 class="heading" style="padding-bottom: 15px;">Hi There! <span class="wave" role="img" aria-labelledby="wave">👋🏻</span></h1>
+        <h1 class="heading-name">I'M<strong class="main-name"> Bogdan Vaskan</strong></h1>
+        <div class="pt-2" style="text-align: left;">
+          <div class="Typewriter d-flex" data-testid="typewriter-wrapper">
+            <VueTyped
+              :strings="typedStrings"
+              :typeSpeed="40"
+              :backSpeed="30"
+              :loop="true"
+              @onComplete="handleComplete"
+            ></VueTyped>
+            <span class="Typewriter__cursor">|</span>
           </div>
         </div>
-        <div class="col-md-5" style="padding-bottom: 20px;">
-          <img src="/src/assets/images/header_image.svg" alt="home pic" class="img-fluid" style="max-height: 450px;">
-        </div>
       </div>
+      <div class="col-md-5" style="padding-bottom: 20px;">
+        <img src="/src/assets/images/header_image.svg" alt="home pic" class="img-fluid" style="max-height: 450px;">
+      </div>
+    </div>
 
-    <div class="row">
+    <div class="row pb-5 pt-5" id="about-section">
       <div class="col-7">
         <h1 style="font-size: 2.6em;">LET ME <span class="purple"> INTRODUCE </span> MYSELF</h1>
         <p>Skilled Software Engineer with hands-on experience in both
