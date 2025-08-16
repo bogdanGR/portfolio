ddc08eaf5d26eeac9a095791f943fc614c15fb19
bogdanGR <fant1k450@gmail.com>
2025-08-16 13:42:51 +0300

    Add base components and register them globally

--- src/main.js
+++ src/main.js
@@ -2,21 +2,29 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap';
 import './assets/main.css'
 // Import Roboto font weights
-import '@fontsource/roboto/300.css'
-import '@fontsource/roboto/400.css'
-import '@fontsource/roboto/500.css'
-import '@fontsource/roboto/700.css'
-import '@fontsource/roboto/900.css'
-import '@fontsource/roboto-mono/400.css'
-import '@fontsource/roboto-mono/500.css'
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import '@fontsource/roboto/900.css';
+import '@fontsource/roboto-mono/400.css';
+import '@fontsource/roboto-mono/500.css';
 
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import BaseCard from '@/components/ui/baseCard.vue';
+import BaseButton from '@/components/ui/baseButton.vue';
+import BaseBadge from '@/components/ui/baseBadge.vue';
 
 
-const app = createApp(App)
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router';
 
-app.use(router)
+const app = createApp(App);
 
-app.mount('#app')
+app.use(router);
+
+app.component('base-card', BaseCard);
+app.component('base-button', BaseButton);
+app.component('base-badge', BaseBadge);
+
+app.mount('#app');
