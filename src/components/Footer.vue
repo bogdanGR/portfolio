40b3c44c99cdab81379c60fee427fa9ef3f87b96
bogdanGR <fant1k450@gmail.com>
2025-08-28 15:42:47 +0300

    Show minimal text on footer section on mobiles

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -6,8 +6,13 @@ import SocialLinks from "@/components/SocialLinks.vue";
   <footer class="footer pt-3 pb-3 border-top border-secondary">
     <div class="container">
       <div class="row align-items-center">
-        <div class="col-6 text-left text-white">
-          Designed and Developed by Bogdan Vaskan. Copyright © {{ new Date().getFullYear() }} BV
+        <div class="col-6 text-start text-white">
+          <span class="d-none d-md-inline">
+            Designed and Developed by Bogdan Vaskan. Copyright © {{ new Date().getFullYear() }} BV
+          </span>
+          <span class="d-inline d-md-none">
+            © {{ new Date().getFullYear() }} BV
+          </span>
         </div>
         <div class="col-6 text-end">
           <div class="d-flex align-items-baseline justify-content-end ">
